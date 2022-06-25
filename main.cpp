76671778d4de58d5858cc7893cde020c5cc50bf1
sukeroV <he429855@gmail.com>
2022-06-25 14:51:00 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hinst, // 实例句柄
+
+                   HINSTANCE hPrevInstance, //上一个应用程序的句柄
+
+                   LPSTR lpCmdLine,//char *argv[]
+
+                   int nCmdShow)//显示命令 最大化 最小化 正常
+{
+
+    //1、设计窗口
+    //2、注册窗口
+    //3、创建窗口
+    //4、显示和更新
+    //5、通过循环获取消息
+    //6、处理消息（窗口过程）
+
+    return 0;
+
+}
+
+
